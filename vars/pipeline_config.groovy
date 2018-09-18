65370e40f2f55a80608f2e8f73cde47e5305e25b
steven-terrana <steventerrana@gmail.com>
2018-09-18 11:27:07 -0700

    Initial commit for SDP

--- vars/pipeline_config.groovy
+++ vars/pipeline_config.groovy
@@ -3,6 +3,6 @@
   This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 */
 
-import sdp.PipelineConfig
+import sdp.config.PipelineConfig
 
-def call(){  return PipelineConfig.instance.config }
+def call(){  return PipelineConfig.instance.current.config }
--- vars/sdp_evaluate.groovy
+++ vars/sdp_evaluate.groovy
@@ -3,7 +3,7 @@
   This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 */
 
-import sdp.SdpBinding
+import sdp.binding.SdpBinding
 
 import org.jenkinsci.plugins.workflow.cps.CpsThreadGroup
 import org.jenkinsci.plugins.workflow.cps.CpsGroovyShell
