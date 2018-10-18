44fb93729db94f883f3d36b79af90d7196364e8f
steven-terrana <steventerrana@gmail.com>
2018-10-18 12:57:48 -0400

    Update static_code_analysis.groovy

--- resources/sdp/libraries/sonarqube/static_code_analysis.groovy
+++ resources/sdp/libraries/sonarqube/static_code_analysis.groovy
@@ -34,12 +34,14 @@ def call(){
           sh script
             
         }
+        /*
         timeout(time: 1, unit: 'HOURS') {
           def qg = waitForQualityGate()
           if (qg.status != 'OK' && enforce) {
             error "Pipeline aborted due to quality gate failure: ${qg.status}"
           }
         }
+        */
       }
     }
   }
